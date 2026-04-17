bf27c5d2f54058e7ee6e3d84b26ce50a160152e0
RumethGunasinghe <rumethgunasingha@gmail.com>
2026-04-17 17:20:32 +1000

    Added real physics animation for the pendulum : not tuned

--- IPwPID.py
+++ IPwPID.py
@@ -7,59 +7,68 @@ from matplotlib.animation import FuncAnimation
 # -------------------------------
 dt = 0.02
 g = 9.81
-length = 1.0
+l = 1.0
+m_c = 1.0
+m_p = 0.1
 
 # PID gains
-Kp = 20
-Kd = 5
+Kp = 100
+Kd = 20
 
-# Initial state
-theta = 1
-theta_dot = 0.0
+# State
+x, x_dot = 0.0, 0.0
+theta, theta_dot = 0.2, 0.0
 
 # -------------------------------
-# Setup plot
+# Plot
 # -------------------------------
 fig, ax = plt.subplots()
-ax.set_xlim(-2, 2)
+ax.set_xlim(-3, 3)
 ax.set_ylim(-1.5, 1.5)
 
-cart_width = 0.4
-cart_height = 0.2
-
-cart = plt.Rectangle((-cart_width/2, -0.1), cart_width, cart_height, color='black')
+cart_w, cart_h = 0.4, 0.2
+cart = plt.Rectangle((0, -0.1), cart_w, cart_h, color='black')
 ax.add_patch(cart)
 
-line, = ax.plot([], [], lw=3)  # pendulum
-time_text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
+pole, = ax.plot([], [], lw=3)
 
 # -------------------------------
-# Animation update
+# Update
 # -------------------------------
 def update(frame):
-    global theta, theta_dot
+    global x, x_dot, theta, theta_dot
 
-    # PID control
+    # Controller (still simple PID)
     force = -Kp * theta - Kd * theta_dot
 
-    # Dynamics
-    theta_ddot = (g / length) * theta + force
+    # Physics
+    sin_t = np.sin(theta)
+    cos_t = np.cos(theta)
+
+    temp = (force + m_p * l * theta_dot**2 * sin_t) / (m_c + m_p)
+
+    theta_ddot = (g * sin_t - cos_t * temp) / (l * (4/3 - (m_p * cos_t**2) / (m_c + m_p)))
+
+    x_ddot = temp - (m_p * l * theta_ddot * cos_t) / (m_c + m_p)
+
+    # Integrate
+    x_dot += x_ddot * dt
+    x += x_dot * dt
 
     theta_dot += theta_ddot * dt
     theta += theta_dot * dt
 
-    # Pendulum position
-    x = length * np.sin(theta)
-    y = length * np.cos(theta)
+    # Draw
+    cart.set_xy((x - cart_w/2, -0.1))
 
-    line.set_data([0, x], [0, y])
-    time_text.set_text(f'theta={theta:.2f}')
+    px = x + l * np.sin(theta)
+    py = l * np.cos(theta)
 
-    return line, time_text
+    pole.set_data([x, px], [0, py])
 
-# -------------------------------
-# Run animation
-# -------------------------------
+    return pole, cart
+
 ani = FuncAnimation(fig, update, frames=500, interval=20)
-plt.title("Inverted Pendulum (PID Control)")
+plt.title("Real Cart-Pole Dynamics (PID)")
+plt.grid()
 plt.show()
