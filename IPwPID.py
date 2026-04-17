df62bfedd6caaf92c5f82d307f35ae5748fc650f
RumethGunasinghe <rumethgunasingha@gmail.com>
2026-04-17 17:15:39 +1000

    added visuals for PID cart  balancing

--- IPwPID.py
+++ IPwPID.py
@@ -1,49 +1,65 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
-# Simulation parameters
-dt = 0.02              # time step
-time = 10              # total simulation time
-steps = int(time / dt)
+# -------------------------------
+# Parameters
+# -------------------------------
+dt = 0.02
+g = 9.81
+length = 1.0
 
-# Pendulum parameters
-g = 9.81               # gravity
-length = 1.0           # length of pendulum
-
-# PID Controller gains
+# PID gains
 Kp = 20
 Kd = 5
 
 # Initial state
-theta = 0.2            # initial angle (radians)
-theta_dot = 0.0        # angular velocity
+theta = 1
+theta_dot = 0.0
 
-# Store results
-theta_list = []
+# -------------------------------
+# Setup plot
+# -------------------------------
+fig, ax = plt.subplots()
+ax.set_xlim(-2, 2)
+ax.set_ylim(-1.5, 1.5)
 
+cart_width = 0.4
+cart_height = 0.2
 
-# Simulation loop
-for i in range(steps):
+cart = plt.Rectangle((-cart_width/2, -0.1), cart_width, cart_height, color='black')
+ax.add_patch(cart)
 
-    #PID control 
+line, = ax.plot([], [], lw=3)  # pendulum
+time_text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
+
+# -------------------------------
+# Animation update
+# -------------------------------
+def update(frame):
+    global theta, theta_dot
+
+    # PID control
     force = -Kp * theta - Kd * theta_dot
 
-    #  Dynamics (simplified) 
+    # Dynamics
     theta_ddot = (g / length) * theta + force
 
-    #  Update state 
     theta_dot += theta_ddot * dt
     theta += theta_dot * dt
 
-    # Store data
-    theta_list.append(theta)
+    # Pendulum position
+    x = length * np.sin(theta)
+    y = length * np.cos(theta)
 
-# Plot results
-time_axis = np.linspace(0, time, steps)
+    line.set_data([0, x], [0, y])
+    time_text.set_text(f'theta={theta:.2f}')
 
-plt.plot(time_axis, theta_list)
-plt.xlabel("Time (s)")
-plt.ylabel("Angle (rad)")
-plt.title("Inverted Pendulum with PID Control")
-plt.grid()
+    return line, time_text
+
+# -------------------------------
+# Run animation
+# -------------------------------
+ani = FuncAnimation(fig, update, frames=500, interval=20)
+plt.title("Inverted Pendulum (PID Control)")
 plt.show()
